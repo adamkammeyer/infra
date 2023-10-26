8c7c0c00f970f73562b10f0665a51c323a68f9aa
Adam Kammeyer <adam@kammeyer.me>
2023-10-26 12:38:49 -0500

    [workstation] Change Ansible install from pip to repos.

--- workstation/ansible.sh
+++ workstation/ansible.sh
@@ -4,25 +4,14 @@ DNF_CMD=$(which dnf)
 APT_CMD=$(which apt)
 
 if [[ ! -z $DNF_CMD ]]; then
-    sudo dnf install -y python3-pip
-    python -m pip install --user ansible
- elif [[ ! -z $APT_CMD ]]; then
-    sudo apt install -y python3-pip pipx
-    pipx ensurepath
-    pipx install ansible
-    pipx ensurepath
-    ln -s ~/.local/pipx/venvs/ansible/bin/ansible ~/.local/bin/ansible
-    ln -s ~/.local/pipx/venvs/ansible/bin/ansible-config ~/.local/bin/ansible-config
-    ln -s ~/.local/pipx/venvs/ansible/bin/ansible-connection ~/.local/bin/ansible-connection
-    ln -s ~/.local/pipx/venvs/ansible/bin/ansible-console ~/.local/bin/ansible-console
-    ln -s ~/.local/pipx/venvs/ansible/bin/ansible-doc ~/.local/bin/ansible-doc
-    ln -s ~/.local/pipx/venvs/ansible/bin/ansible-galaxy ~/.local/bin/ansible-galaxy
-    ln -s ~/.local/pipx/venvs/ansible/bin/ansible-playbook ~/.local/bin/ansible-playbook
-    ln -s ~/.local/pipx/venvs/ansible/bin/ansible-pull ~/.local/bin/ansible-pull
-    ln -s ~/.local/pipx/venvs/ansible/bin/ansible-test ~/.local/bin/ansible-test
-    ln -s ~/.local/pipx/venvs/ansible/bin/ansible-vault ~/.local/bin/ansible-vault
+    sudo dnf install ansible
+elif [[ ! -z $APT_CMD ]]; then
+    sudo apt update
+    sudo apt install software-properties-common
+    sudo add-apt-repository --yes --update ppa:ansible/ansible
+    sudo apt install ansible
 else
-    echo "Error: Unable to install Ansible via pip"
+    echo "Error: Unable to install Ansible"
     exit 1;
 fi
 
